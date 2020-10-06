d2a92c88c46864ea3ed8a8c9419c2dd18f58273d
Lili Huynh <lillianhuynh312@gmail.com>
2020-10-06 14:22:38 -0700

    when user add input (textarea) and click save button

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
 
+    let todoList = [];
+
     //add date and time to header
     $("#currentDay").text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm a'));
+
+    //when user add input (textarea) and click save button:
+    $("button").on("click", function () {
+
+        //define todos: value of textarea- sibling element of the button that is clicked
+        var todos = $(this).siblings("textarea").val();
+        //define hours: text from div- sibling element of the button that is clicked
+        var hours = $(this).siblings("div").text();
+        //creat object 'list': key -hours with value 'hours'; key-todos with value 'todos'
+        var list = { "hours": hours, "todos": todos };
+        //add new object to 'todoList'
+        todoList.push(list);
+
+        //localstorage 'todoList'
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+    })
+
 })
