634c90e4f468137c164009cd5705143ed7a26f36
Lili Huynh <lillianhuynh312@gmail.com>
2020-10-08 12:48:53 -0700

    fixed else statement for houroperation fuction

--- assets/script.js
+++ assets/script.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
         $(".textarea").addClass("present");
 
       } else {
-        $(this).addClass("future");
+        $(".textarea").addClass("future");
       }
 
     });
